5506eddd876a1606184a115872667bb0a495e189
Robert Grant <rhgrant10@gmail.com>
2018-06-10 11:49:13 -0700

    Add intersphinx and use rtd-theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,11 @@ import berserk
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -84,7 +88,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -160,4 +165,8 @@ texinfo_documents = [
 ]
 
 
-
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'http://docs.python.org/3/': None,
+    'http://docs.python-requests.org/en/latest/': None
+}
