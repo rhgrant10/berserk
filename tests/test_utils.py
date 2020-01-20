ef3a475262d45bc1e8a5035ea4525fa7a02ed1d3
Robert Grant <rhgrant10@gmail.com>
2020-01-19 20:52:20 -0800

    Fix broken unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,8 +9,8 @@ def test_datetime_from_millis():
     assert dt.timestamp() == 1514505150.384
 
 
-def test_inner_datetime_fromtimestamp():
-    converter = utils.inner_datetime_fromtimestamp('foo')
+def test_inner_datetime_from_seconds():
+    converter = utils.inner(utils.datetime_from_seconds, 'foo')
     result = converter({'foo': 1514505150, 'bar': 'baz', 'qux': 123})
     assert result == {
         'foo': datetime.datetime(2017, 12, 28, 23, 52, 30,
