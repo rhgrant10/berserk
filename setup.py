30dd17fc00dc2a09c67fc0b911e9a98fad998284
Robert Grant <rhgrant10@gmail.com>
2022-04-10 13:13:52 -0700

    Migrate setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,46 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+import setuptools
 
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-with open('requirements.txt') as requirements_file:
-    requirements = requirements_file.read().splitlines()
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest', ]
-
-setup(
-    author="Robert Grant",
-    author_email='rhgrant10@gmail.com',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description="Python client for the lichess API",
-    install_requires=requirements,
-    license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/x-rst',
-    include_package_data=True,
-    keywords='berserk',
-    name='berserk',
-    packages=find_packages(include=['berserk']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/rhgrant10/berserk',
-    version='0.10.0',
-    zip_safe=True,
-)
+setuptools.setup()
